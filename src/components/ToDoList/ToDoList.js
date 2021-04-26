7027289f67e24256bebd7725085e1367073ba419
Delta de Dirac <igorshevtsov@gmail.com>
2021-04-26 17:57:02 +0300

    fix background image position

--- src/components/ToDoList/ToDoList.js
+++ src/components/ToDoList/ToDoList.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./ToDoList.css";
-import { ListGroup } from "react-bootstrap";
+import { Container, ListGroup } from "react-bootstrap";
 
 export default function ToDoList() {
   return (
