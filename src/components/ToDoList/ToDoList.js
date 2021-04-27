692466c8f1f5eb1e3e876eb2d5529ef29402e218
Delta de Dirac <igorshevtsov@gmail.com>
2021-04-27 19:51:21 +0300

    handle ToDo item removal

--- src/components/ToDoList/ToDoList.js
+++ src/components/ToDoList/ToDoList.js
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import React, { useState, createRef } from "react";
+import { findDOMNode } from "react-dom";
 import "./ToDoList.css";
 import { v4 as uuidv4 } from "uuid";
 import { ListGroup } from "react-bootstrap";
@@ -8,18 +9,62 @@ import { faTimes } from "@fortawesome/free-solid-svg-icons";
 export default function ToDoList({ todoList, toggleTodo, filter, removeTodo }) {
   const [toBeRemoved, setToBeRemoved] = useState(null);
 
+  function findID(e) {
+    // console.log(e);
+    let id = null;
+    const mouseOverNode = e.target;
+    if (mouseOverNode.nodeName === "DIV") {
+      if (mouseOverNode.id.split(";")[1]) {
+        id = mouseOverNode.id.split(";")[1];
+        // console.log("found DIV ID:", mouseOverNode.id.split(";")[1]);
+      } else {
+        const relTargetInput = e.relatedTarget;
+        if (relTargetInput.nodeName === "DIV") {
+          id = relTargetInput.id.split(";")[1];
+          // console.log("found DIV related DIV ID:", relTargetInput.id.split(";")[1]);
+        } else if (relTargetInput.nodeName === "INPUT") {
+          id = relTargetInput.id;
+          // console.log("found DIV related INPUT ID:", relTargetInput.id);
+        } else if (relTargetInput.nodeName === "LABEL") {
+          id = relTargetInput.htmlFor;
+          // console.log("found DIV related LABEL HTMLFOR:", relTargetInput.htmlFor);
+        }
+      }
+    } else if (mouseOverNode.nodeName === "INPUT") {
+      id = mouseOverNode.id;
+      // console.log("found INPUT ID:", mouseOverNode.id);
+    } else if (mouseOverNode.nodeName === "LABEL") {
+      id = mouseOverNode.htmlFor;
+      // console.log("found LABEL HTMLFOR:", mouseOverNode.htmlFor);
+    } else if (mouseOverNode.nodeName === "svg") {
+      // console.log("SVG", toBeRemoved);
+      id = toBeRemoved;
+    } else {
+      // console.log("not found", e);
+      id = toBeRemoved;
+    }
+
+    return id;
+  }
+
   return (
     <>
       <ListGroup variant="flush" className="Todo-list">
         {todoList
           .filter((todo) => (filter === "all" ? true : filter === "active" ? !todo.isCompleted : todo.isCompleted))
           .map((todo) => (
-            <ListGroup.Item variant="dark" key={uuidv4()}>
-              <div
-                className="form-check form-switch"
-                onMouseEnter={(e) => setToBeRemoved(e.target.children.length ? e.target.children[0].id : null)}
-                onMouseLeave={() => setToBeRemoved(null)}
-              >
+            <ListGroup.Item
+              variant="dark"
+              key={uuidv4()}
+              id={`lgItem;${todo.id}`}
+              onMouseOver={(e) => setToBeRemoved(findID(e))}
+            >
+              {toBeRemoved === todo.id ? (
+                <FontAwesomeIcon className="showIcon" icon={faTimes} onClick={() => removeTodo(toBeRemoved)} />
+              ) : (
+                <FontAwesomeIcon className="hideIcon" icon={faTimes} onClick={() => removeTodo(toBeRemoved)} />
+              )}
+              <div className="form-check form-switch">
                 <input
                   className="form-check-input"
                   type="checkbox"
@@ -31,11 +76,6 @@ export default function ToDoList({ todoList, toggleTodo, filter, removeTodo }) {
                 <label className={"form-check-label ".concat(todo.isCompleted ? "completed" : "")} htmlFor={todo.id}>
                   {todo.text}
                 </label>
-                <span className="deleteToDo">
-                  {toBeRemoved === todo.id ? (
-                    <FontAwesomeIcon icon={faTimes} onClick={() => removeTodo(toBeRemoved)} />
-                  ) : null}
-                </span>
               </div>
             </ListGroup.Item>
           ))}
